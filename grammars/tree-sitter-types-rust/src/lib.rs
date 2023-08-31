5db3f6be37c6627cfb9cdaca0779cc0ad49d9764
Quinn Parrott <quinn.parrott@gmail.com>
2023-08-31 13:26:17 -0400

    Abbreviated re-export names

--- grammars/tree-sitter-types-rust/src/lib.rs
+++ grammars/tree-sitter-types-rust/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(non_camel_case_types)]
 
-pub use tree_sitter_rust;
-pub use tree_sitter_types_common::{self, tree_sitter};
+pub use tree_sitter_rust as ts_rust;
+pub use tree_sitter_types_common::{self as ts_common, tree_sitter as ts};
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
